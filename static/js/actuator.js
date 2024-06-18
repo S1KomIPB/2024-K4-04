acb0c31eeb60eb5f61cf5f63f896c8054fe0fec1
Fahmi Ihsan <7c.fahmi@gmail.com>
2024-06-18 21:46:05 +0700

    melody actuator

--- static/js/actuator.js
+++ static/js/actuator.js
@@ -40,3 +40,39 @@ function fetchLedOff() {
             document.getElementById('content').innerText = 'Fetch error: ' + error;
         });
 }
+
+function fetchMelodyA() {
+    const ipAddress = getIpAddress();
+    if (!ipAddress) {
+        document.getElementById('content').innerText = 'Please enter a valid IP address.';
+        return;
+    }
+    fetch(`http://${ipAddress}/melodyA`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            document.getElementById('content').innerText = 'Melody A is on';
+        })
+        .catch(error => {
+            document.getElementById('content').innerText = 'Fetch error: ' + error;
+        });
+}
+
+function fetchMelodyB() {
+    const ipAddress = getIpAddress();
+    if (!ipAddress) {
+        document.getElementById('content').innerText = 'Please enter a valid IP address.';
+        return;
+    }
+    fetch(`http://${ipAddress}/melodyB`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            document.getElementById('content').innerText = 'Melody B is on';
+        })
+        .catch(error => {
+            document.getElementById('content').innerText = 'Fetch error: ' + error;
+        });
+}
